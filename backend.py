921eb4d186b30bf87a3eed0eb08e9f67a095d3b8
MoyezM <moyez.mansoor@gmail.com>
2020-06-02 17:01:09 -0400

    added more stuff for backend

---
+++ backend.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_hub as hub
+import pickle
+
+tf.config.set_visible_devices([], 'GPU')
+
+model = keras.models.load_model('model_data/model.h5')
+
+with open('model_data/corpus.pickle', 'rb') as corpus:
+    word_index, index_word = pickle.load(corpus)
+
+url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"
+embed = hub.load(url)
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/predict", methods = ['GET'])
+def predict():
+    text = request.args.get('text')
+
+    embedding = embed([text])
+    
+    pred = model.predict(embedding)
+    
+    idx = np.argmax(pred)
+    
+    return index_word[idx]
+    
--- flaskapp.py
+++ flaskapp.py
@@ -1,34 +1,9 @@
 from flask import Flask
-from flask import request
-from flask_cors import CORS
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-import tensorflow_hub as hub
-import pickle
-
-tf.config.set_visible_devices([], 'GPU')
-
-model = keras.models.load_model('model_data/model.h5')
-
-with open('model_data/corpus.pickle', 'rb') as corpus:
-    word_index, index_word = pickle.load(corpus)
-
-url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"
-embed = hub.load(url)
-
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/predict", methods = ['GET'])
-def predict():
-    text = request.args.get('text')
+@app.route('/')
+def hello_world():
+  return 'Hello from Flask!'
 
-    embedding = embed([text])
-    
-    pred = model.predict(embedding)
-    
-    idx = np.argmax(pred)
-    
-    return index_word[idx]
-    
+if __name__ == '__main__':
+  app.run()
