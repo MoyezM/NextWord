316ee3b18410a3a40093d5f82f8cdcb2f97d0ac2
MoyezM <moyez.mansoor@gmail.com>
2020-06-02 17:17:11 -0400

    stuff for server

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,16 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+import requests 
 
-@app.route('/')
-def hello_world():
-  return 'Hello from Flask!'
+app = Flask(__name__)
+@app.route("/predict", methods = ['GET'])
+def predict():
+    text = request.args.get('text')
+    URL = 'http://127.0.0.1:8050/predict'
+    PARAMS = {'text':text}
+    r = requests.get(url = URL, params = PARAMS) 
+    return r.text
+    
 
 if __name__ == '__main__':
   app.run()
